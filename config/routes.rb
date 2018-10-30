56d18e000f83415df31257244f7dbf71a31ede97
Michael Wong <mail@michaelwongsilversmith.com>
2018-10-30 14:47:15 +0000

    view api v1 restaurants generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :restaurants, only: [:index]
     end
